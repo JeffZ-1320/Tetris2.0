1a495ad18cb515d0ef8a90ffd8a6ad09172c6fa4
JeffZ-1320 <76709031+JeffZ-1320@users.noreply.github.com>
2021-03-14 20:32:19 -0400

    Set up tetris homepage

--- src/main/java/com/tetris/Tetris20/Application.java
+++ src/main/java/com/tetris/Tetris20/Application.java
@@ -2,18 +2,23 @@ package com.tetris.Tetris20;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	@RequestMapping(value = "/")
-	public String hello(){
-		return "Hello World";
+	@GetMapping("/")
+	public String home(){
+		return "/html/index";
 	}
 }
